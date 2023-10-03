45b56ef58814d4f994e93a3c06dad31d994e4225
davydenkomax <davydenko.3x3x3@gmail.com>
2023-10-03 17:54:09 +0700

    Добавьте файлы проекта.

---
+++ L3_2/L3_2.cpp
@@ -0,0 +1,26 @@
+﻿// 2. Даны два числа (вещественные). Если первое число положительное или равно нулю, то умножить второе число на 1000, иначе — разделить на 10.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "ru");
+
+    double number1, number2;
+    cout << "Введите первое число: ";
+    cin >> number1;
+    cout << "Введите второе число: ";
+    cin >> number2;
+
+    // Проверка number1 на неотрицательность
+    if (number1 >= 0) {
+        cout << "Ответ: " << number2 * 1000;
+    }
+    else {
+        cout << "Ответ: " << number2 / 10;
+    }
+
+    return 0;
+}
